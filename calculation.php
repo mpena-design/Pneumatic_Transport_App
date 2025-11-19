e377b8d3be09e2c2d823a72ff79228c4b6abbacc
mpena-design <m.pena@heptapro.com>
2025-11-19 07:56:16 -0500

    Update calculation.php
    Fix critical bug: pressure adjustment must increase when non-physical (line 392)

--- calculation.php
+++ calculation.php
@@ -389,7 +389,7 @@ class PneumaticCalculator {
                 // Captura la excepción de "presión no física"
                 if (strpos($e->getMessage(), 'no física') !== false) {
                     // Hacer un salto más grande para converger más rápido en sistemas largos
-                    $Preq_bar_guess = $Preq_bar_guess - 0.1;
+                    $Preq_bar_guess = $Preq_bar_guess + 0.1;
                     $last_error = 0; // Resetear el solver
                     $last_guess = 0;
                     if ($Preq_bar_guess > 10) { // Failsafe para evitar bucles infinitos
@@ -594,6 +594,7 @@ class PneumaticCalculator {
         }
     }
 }
+
 
 
 
